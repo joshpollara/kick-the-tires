363618c0732a1c340d4318adc560567947f6735c
Josh Pollara <josh@terrateam.io>
2026-07-22 07:11:52 +0000

    Demo: phase2 terragrunt engine structured counts

---
+++ tg/main.tf
@@ -0,0 +1,5 @@
+terraform { required_version = ">= 1.4" }
+resource "terraform_data" "alpha" { input = "1" }
+resource "terraform_data" "beta"  { input = "1" }
+resource "terraform_data" "gamma" { input = "1" }
+resource "terraform_data" "delta" { input = "1" }
---
+++ tg/terragrunt.hcl
@@ -0,0 +1,1 @@
+# minimal terragrunt module (local state)
