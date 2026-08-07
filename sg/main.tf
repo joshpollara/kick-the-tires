e6cfc3268c91960b75d5067078be0c06caaee345
joshpollara <75546+joshpollara@users.noreply.github.com>
2026-08-07 12:18:43 +0200

    Create main.tf

---
+++ sg/main.tf
@@ -0,0 +1,24 @@
+resource "terraform_data" "vpc" {
+  input = {
+    vpc_id             = "vpc-0a1b2c3d4e5f6789"
+    cidr_block         = "10.0.0.0/16"
+    public_subnet_ids  = ["subnet-pub-a", "subnet-pub-b"]
+    private_subnet_ids = ["subnet-prv-a", "subnet-prv-b", "subnet-prv-c"]
+  }
+}
+
+output "vpc_id" {
+  value = terraform_data.vpc.output.vpc_id
+}
+
+output "vpc_cidr_block" {
+  value = terraform_data.vpc.output.cidr_block
+}
+
+output "public_subnet_ids" {
+  value = terraform_data.vpc.output.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = terraform_data.vpc.output.private_subnet_ids
+}
