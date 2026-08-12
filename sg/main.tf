da6aa761589293b01d67743ce590607ff28752d9
Josh Pollara <josh@terrateam.io>
2026-08-12 10:49:12 +0000

    Exercise stacks: three-stack dependency chain (dev -> staging -> sg)

--- sg/main.tf
+++ sg/main.tf
@@ -2,6 +2,7 @@ resource "terraform_data" "vpc" {
   input = {
     vpc_id             = "vpc-0a1b2c3d4e5f6789"
     cidr_block         = "10.0.0.0/16"
+    environment        = "stacks-demo"
     public_subnet_ids  = ["subnet-pub-a", "subnet-pub-b"]
     private_subnet_ids = ["subnet-prv-a", "subnet-prv-b", "subnet-prv-c"]
   }
